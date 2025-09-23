1fc25ebfea317e551a880699234c303202211ac7
Manish Singh <manish.singh@thoughtfocus.com>
2025-09-23 18:57:05 +0530

    Change DPDP hero background to lightish orange
    - Changed gradient from red tones to orange/amber: orange-600/orange-500/amber-600
    - Provides comfortable, light orange background
    - Maintains gradient structure while improving visual comfort

--- src/components/dpdp-compliance/DpdpHero.tsx
+++ src/components/dpdp-compliance/DpdpHero.tsx
@@ -5,7 +5,7 @@ import { ShieldCheckIcon, ScaleIcon, DocumentTextIcon, ExclamationTriangleIcon }
 
 export default function DpdpHero() {
   return (
-    <div className="relative isolate bg-gradient-to-br from-red-700 via-red-600 to-rose-700">
+    <div className="relative isolate bg-gradient-to-br from-orange-600 via-orange-500 to-amber-600">
       {/* Background Pattern */}
       <div className="absolute inset-0 -z-10 overflow-hidden">
         <div className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 dark:stroke-gray-700 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]">
